bcd4082f29e9406fdfc132d69a59b6b030fb1d5c
MrExplode <me@sunstorm.rocks>
2022-03-26 16:12:55 +0100

    fix indentation on drill 16

--- src/drill/week05/Lines_window.cpp
+++ src/drill/week05/Lines_window.cpp
@@ -2,57 +2,57 @@
 
 Lines_window::Lines_window(Point xy, int w, int h, const string &title)
     : Window{xy, w, h, title},
-      next_button{Point{x_max() - 150, 0}, 70, 20, "Next point", [](Address, Address pw) { reference_to<Lines_window>(pw).next(); }},
-      quit_button{Point{x_max() - 70, 0}, 70, 20, "Quit", [](Address, Address pw) { reference_to<Lines_window>(pw).quit(); }},
-      next_x{Point{x_max() - 310, 0}, 50, 20, "next x:"},
-      next_y{Point{x_max() - 210, 0}, 50, 20, "next y:"},
-      xy_out{Point{100, 0}, 100, 20, "current (x,y):"},
-      // coloring
-      color_menu{Point{x_max() - 70, 25}, 70, 20, Menu::vertical, "color"},
-      menu_button{Point{x_max() - 80, 25}, 80, 20, "color menu", [](Address, Address pw) { reference_to<Lines_window>(pw).color_pressed(); }},
-      // styling
-      style_menu{Point{x_max() - 70, 55}, 70, 20, Menu::vertical, "style"},
-      style_button{Point{x_max() - 80, 55}, 80, 20, "style menu", [](Address, Address pw) { reference_to<Lines_window>(pw).style_pressed(); }},
-      style_open{false}
+        next_button{Point{x_max() - 150, 0}, 70, 20, "Next point", [](Address, Address pw) { reference_to<Lines_window>(pw).next(); }},
+        quit_button{Point{x_max() - 70, 0}, 70, 20, "Quit", [](Address, Address pw) { reference_to<Lines_window>(pw).quit(); }},
+        next_x{Point{x_max() - 310, 0}, 50, 20, "next x:"},
+        next_y{Point{x_max() - 210, 0}, 50, 20, "next y:"},
+        xy_out{Point{100, 0}, 100, 20, "current (x,y):"},
+        // coloring
+        color_menu{Point{x_max() - 70, 25}, 70, 20, Menu::vertical, "color"},
+        menu_button{Point{x_max() - 80, 25}, 80, 20, "color menu", [](Address, Address pw) { reference_to<Lines_window>(pw).color_pressed(); }},
+        // styling
+        style_menu{Point{x_max() - 70, 55}, 70, 20, Menu::vertical, "style"},
+        style_button{Point{x_max() - 80, 55}, 80, 20, "style menu", [](Address, Address pw) { reference_to<Lines_window>(pw).style_pressed(); }},
+        style_open{false}
 
 {
-  attach(next_button);
-  attach(quit_button);
-  attach(next_x);
-  attach(next_y);
-  attach(xy_out);
-  attach(lines);
-
-  // coloring
-  color_menu.attach(new Button{Point{0, 0}, 0, 0, "red", [](Address, Address pw) { reference_to<Lines_window>(pw).red_pressed(); }});
-  color_menu.attach(new Button{Point{0, 0}, 0, 0, "blue", [](Address, Address pw) { reference_to<Lines_window>(pw).blue_pressed(); }});
-  color_menu.attach(new Button{Point{0, 0}, 0, 0, "black", [](Address, Address pw) { reference_to<Lines_window>(pw).black_pressed(); }});
-  attach(color_menu);
-  color_menu.hide();
-  attach(menu_button);
-
-  // styling
-  style_menu.attach(new Button{Point{0, 0}, 0, 0, "solid", [](Address, Address pw) { reference_to<Lines_window>(pw).solid_pressed(); }});
-  style_menu.attach(new Button{Point{0, 0}, 0, 0, "dash", [](Address, Address pw) { reference_to<Lines_window>(pw).dash_pressed(); }});
-  style_menu.attach(new Button{Point{0, 0}, 0, 0, "dot", [](Address, Address pw) { reference_to<Lines_window>(pw).dot_pressed(); }});
-  style_menu.attach(new Button{Point{0, 0}, 0, 0, "- .", [](Address, Address pw) { reference_to<Lines_window>(pw).dashdot_pressed(); }});
-  style_menu.attach(new Button{Point{0, 0}, 0, 0, "- . .", [](Address, Address pw) { reference_to<Lines_window>(pw).dashdotdot_pressed(); }});
-  attach(style_menu);
-  style_menu.hide();
-  attach(style_button);
+    attach(next_button);
+    attach(quit_button);
+    attach(next_x);
+    attach(next_y);
+    attach(xy_out);
+    attach(lines);
+  
+    // coloring
+    color_menu.attach(new Button{Point{0, 0}, 0, 0, "red", [](Address, Address pw) { reference_to<Lines_window>(pw).red_pressed(); }});
+    color_menu.attach(new Button{Point{0, 0}, 0, 0, "blue", [](Address, Address pw) { reference_to<Lines_window>(pw).blue_pressed(); }});
+    color_menu.attach(new Button{Point{0, 0}, 0, 0, "black", [](Address, Address pw) { reference_to<Lines_window>(pw).black_pressed(); }});
+    attach(color_menu);
+    color_menu.hide();
+    attach(menu_button);
+  
+    // styling
+    style_menu.attach(new Button{Point{0, 0}, 0, 0, "solid", [](Address, Address pw) { reference_to<Lines_window>(pw).solid_pressed(); }});
+    style_menu.attach(new Button{Point{0, 0}, 0, 0, "dash", [](Address, Address pw) { reference_to<Lines_window>(pw).dash_pressed(); }});
+    style_menu.attach(new Button{Point{0, 0}, 0, 0, "dot", [](Address, Address pw) { reference_to<Lines_window>(pw).dot_pressed(); }});
+    style_menu.attach(new Button{Point{0, 0}, 0, 0, "- .", [](Address, Address pw) { reference_to<Lines_window>(pw).dashdot_pressed(); }});
+    style_menu.attach(new Button{Point{0, 0}, 0, 0, "- . .", [](Address, Address pw) { reference_to<Lines_window>(pw).dashdotdot_pressed(); }});
+    attach(style_menu);
+    style_menu.hide();
+    attach(style_button);
 }
 
 void Lines_window::quit() { hide(); }
 
 void Lines_window::next() {
-  int x = next_x.get_int();
-  int y = next_y.get_int();
-
-  lines.add(Point{x, y});
-
-  ostringstream ss;
-  ss << '(' << x << ',' << y << ')';
-  xy_out.put(ss.str());
-
-  redraw();
+    int x = next_x.get_int();
+    int y = next_y.get_int();
+  
+    lines.add(Point{x, y});
+  
+    ostringstream ss;
+    ss << '(' << x << ',' << y << ')';
+    xy_out.put(ss.str());
+  
+    redraw();
 }
--- src/drill/week05/Lines_window.h
+++ src/drill/week05/Lines_window.h
@@ -3,110 +3,110 @@
 using namespace Graph_lib;
 
 struct Lines_window : Graph_lib::Window {
-	Lines_window(Point xy, int w, int h, const string &title);
+    Lines_window(Point xy, int w, int h, const string &title);
 
 private:
-	Open_polyline lines;
+    Open_polyline lines;
 
-	Button next_button;
-	Button quit_button;
+    Button next_button;
+    Button quit_button;
 
-	In_box next_x;
-	In_box next_y;
+    In_box next_x;
+    In_box next_y;
 
-	Out_box xy_out;
+    Out_box xy_out;
 
-	// coloring options
-	Menu color_menu;
-	Button menu_button;
+    // coloring options
+    Menu color_menu;
+    Button menu_button;
 
-	// styling options
-	Menu style_menu;
-	Button style_button;
-	// track menu state
-	bool style_open;
+    // styling options
+    Menu style_menu;
+    Button style_button;
+    // track menu state
+    bool style_open;
 
-	void next();
-	void quit();
+    void next();
+    void quit();
 
-	bool wait_for_button();
+    bool wait_for_button();
 
-	void change(Color c) { lines.set_color(c); }
-	void change_style(Line_style s) { lines.set_style(s); }
+    void change(Color c) { lines.set_color(c); }
+    void change_style(Line_style s) { lines.set_style(s); }
 
-	void hide_color_menu() {
-		color_menu.hide();
-		menu_button.show();
-		style_button.move(0, -50);
-		style_menu.move(0, -50);
-		// on move the widget resets to visible
-		if (!style_open)
-			style_menu.hide();
-		else
-			style_button.hide();
-	}
+    void hide_color_menu() {
+        color_menu.hide();
+        menu_button.show();
+        style_button.move(0, -50);
+        style_menu.move(0, -50);
+        // on move the widget resets to visible
+        if (!style_open)
+            style_menu.hide();
+        else
+            style_button.hide();
+    }
 
-	void hide_style_menu() {
-		style_menu.hide();
-		style_button.show();
-		style_open = false;
-	}
+    void hide_style_menu() {
+        style_menu.hide();
+        style_button.show();
+        style_open = false;
+    }
 
-	void red_pressed() {
-		change(Color::red);
-		hide_color_menu();
-	}
+    void red_pressed() {
+        change(Color::red);
+        hide_color_menu();
+    }
 
-	void blue_pressed() {
-		change(Color::blue);
-		hide_color_menu();
-	}
+    void blue_pressed() {
+        change(Color::blue);
+        hide_color_menu();
+    }
 
-	void black_pressed() {
-		change(Color::black);
-		hide_color_menu();
-	}
+    void black_pressed() {
+        change(Color::black);
+        hide_color_menu();
+    }
 
-	void color_pressed() {
-		menu_button.hide();
-		color_menu.show();
+    void color_pressed() {
+        menu_button.hide();
+        color_menu.show();
 
-		style_button.move(0, 50);
-		style_menu.move(0, 50);
-		if (!style_open)
-			style_menu.hide();
-		else
-			style_button.hide();
-	}
+        style_button.move(0, 50);
+        style_menu.move(0, 50);
+        if (!style_open)
+            style_menu.hide();
+        else
+            style_button.hide();
+    }
 
-	void solid_pressed() {
-		change_style(Line_style::solid);
-		hide_style_menu();
-	}
+    void solid_pressed() {
+        change_style(Line_style::solid);
+        hide_style_menu();
+    }
 
-	void dash_pressed() {
-		change_style(Line_style::dash);
-		hide_style_menu();
-	}
+    void dash_pressed() {
+        change_style(Line_style::dash);
+        hide_style_menu();
+    }
 
-	void dot_pressed() {
-		change_style(Line_style::dot);
-		hide_style_menu();
-	}
+    void dot_pressed() {
+        change_style(Line_style::dot);
+        hide_style_menu();
+    }
 
-	void dashdot_pressed() {
-		change_style(Line_style::dashdot);
-		hide_style_menu();
-	}
+    void dashdot_pressed() {
+        change_style(Line_style::dashdot);
+        hide_style_menu();
+    }
 
-	void dashdotdot_pressed() {
-		change_style(Line_style::dashdotdot);
-		hide_style_menu();
-	}
+    void dashdotdot_pressed() {
+        change_style(Line_style::dashdotdot);
+        hide_style_menu();
+    }
 
-	void style_pressed() {
-		style_button.hide();
-		style_menu.show();
-		style_open = true;
-	}
+    void style_pressed() {
+        style_button.hide();
+        style_menu.show();
+        style_open = true;
+    }
 };
