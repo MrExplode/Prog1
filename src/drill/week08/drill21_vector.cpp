aca7c2da2038344054d0428bb4f43c746d3e6c89
MrExplode <me@sunstorm.rocks>
2022-04-06 11:36:04 +0200

    Update drill21
    Replace traditional loop-based solutions with std functions

--- src/drill/week08/drill21_vector.cpp
+++ src/drill/week08/drill21_vector.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <numeric>
 #include <algorithm>
 #include <functional>
 
@@ -25,23 +26,25 @@ int main() {
     
     std::cout << vd << std::endl;
 
-    vector<int> vi;
-    for (double d : vd)
-        vi.push_back(d);
+    vector<int> vi (vd.size());
+    //for (double d : vd)
+    //    vi.push_back(d);
+    std::copy(vd.begin(), vd.end(), vi.begin());
 
     std::cout << "pairs of elements: " << std::endl;
     for (int i = 0; i < vd.size(); i++)
         std::cout << vd[i] << "\t: " << vi[i] << std::endl;
 
-    double doubleSum = 0;
-    for (double d : vd)
-        doubleSum += d;
+    double doubleSum = std::accumulate(vd.begin(), vd.end(), 0.0);
+    //for (double d : vd)
+    //    doubleSum += d;
     std::cout << std::endl << "Sum of double vector: " << doubleSum << std::endl << std::endl;
 
-    int intSum = 0;
-    for (int i : vi)
-        intSum += i;
-    std::cout << "Difference of sums: " << (doubleSum - intSum) << std::endl << std::endl;
+    int intSum = std::accumulate(vi.begin(), vi.end(), 0);
+    //for (int i : vi)
+    //    intSum += i;
+    double diff_sum = std::inner_product(vd.begin(), vd.end(), vi.begin(), 0.0, std::plus<double>(), std::minus<double>());
+    std::cout << "Difference of sums: " << (doubleSum - intSum) << " " << diff_sum << std::endl << std::endl;
 
     std::reverse(vd.begin(), vd.end());
     std::cout << "Reverse double vector: " << vd << std::endl << std::endl;
@@ -50,7 +53,8 @@ int main() {
     std::cout << "Double vector mean: " << vdMean << std::endl << std::endl;
 
     vector<double> vd2;
-    std::copy_if(vd.begin(), vd.end(), vd2.begin(), [vdMean](double value) { return value < vdMean; });
+    auto it = std::copy_if(vd.begin(), vd.end(), vd2.begin(), [vdMean](double value) { return value < vdMean; });
+    vd2.resize(std::distance(vd2.begin(), it));
     std::cout << "Values below mean: " << vd2 << std::endl << std::endl;
 
     std::sort(vd.begin(), vd.end());
